27a68a1122c1082db6b958b4317ce3b3297c75ed
ooflet <ooflet@proton.me>
2025-05-03 14:21:53 +1000

    Use project name rather than file name on the outputted binary

--- src/utils/project.py
+++ src/utils/project.py
@@ -648,10 +648,10 @@ class FprjProject:
 
         if platform == "Windows":
             process.setProgram(compilerLocation)
-            process.setArguments(["-b", path.replace("/", "\\"), location.replace("/", "\\"), str.split(os.path.basename(path), ".")[0]+".face", "1461256429"])
+            process.setArguments(["-b", path.replace("/", "\\"), location.replace("/", "\\"), self.getTitle()+".face", "1461256429"])
         elif platform == "Linux":
             process.setProgram(which("wine"))
-            process.setArguments([compilerLocation, "-b", path.replace("/", "\\"), location.replace("/", "\\"), str.split(os.path.basename(path), ".")[0]+".face", "1461256429"])
+            process.setArguments([compilerLocation, "-b", path.replace("/", "\\"), location.replace("/", "\\"), self.getTitle()+".face", "1461256429"])
         else:
             return False, "Platform not implemented"
 
